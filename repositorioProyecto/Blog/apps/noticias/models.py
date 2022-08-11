eda41ca04ed987f32c0d5901b777c96d45b84506
MaroSand <110480647+MaroSand@users.noreply.github.com>
2022-08-10 21:39:00 -0300

    Update models.py

--- repositorioProyecto/Blog/apps/noticias/models.py
+++ repositorioProyecto/Blog/apps/noticias/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Noticia(models.Model):
+	titulo = models.CharField(max_length = 120)
+	creado = models.DateField(auto_now_add = True)
+	cuerpo = models.TextField()
+	autor = models.CharField(max_length = 50, null= True)
+	imagen = models.ImageField(upload_to = 'noticias', null = True)
+
+	def __str__(self):
+		return self.titulo
