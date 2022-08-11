b4baf07ea4d3b3f0785cc0ae9eac6fc6b18e3ef6
Diego Salazar <109871674+Dieguete97@users.noreply.github.com>
2022-08-10 22:37:32 -0300

    se agrego nuevos cambios

--- repositorioProyecto/Blog/apps/noticias/views.py
+++ repositorioProyecto/Blog/apps/noticias/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Noticia
+
 def Listar(request):
-    return render(request,'noticias/listar_noticias.html')
-# Create your views here.
+	#Creo el diccionario para pasar datos al temaplte
+	ctx = {}	
+	#BUSCAR LO QUE QUIERO EN LA BD
+	todas = Noticia.objects.all()
+	#PASARLO AL TEMPLATE
+	ctx['notis'] = todas
+
+	return render(request,'noticias/listar_noticias.html',ctx)
+
+# EJEMPLO DE COMO DESARMA EL CTX EL TEMPLATE.
+# ctx['nombre'] = 'nicolas'
+# ctx['notas'] = [5,6,9]
+# EL TEMPLATE ya separa el diccionario
+# nombre = 'nicolas'
+# notas = [5,6,9]
