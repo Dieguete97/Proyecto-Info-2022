df2439126be2d65629a6eb98232f990314df1105
Diego Salazar <109871674+Dieguete97@users.noreply.github.com>
2022-08-21 18:51:49 -0300

    Se actualizo las clases pasadas

--- repositorioProyecto/Blog/apps/noticias/urls.py
+++ repositorioProyecto/Blog/apps/noticias/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "noticias"
 urlpatterns = [
    
-    path('listar', views.Listar, name = 'listar_noticias'),
+    path('listar/', views.Listar, name = 'listar_noticias'),
 
-   
+    path('detalle/<int:pk>', views.Detalle_Noticia_Clase.as_view(), name = 'detalle_noticia'),
+
 ]
 
--- repositorioProyecto/Blog/apps/noticias/views.py
+++ repositorioProyecto/Blog/apps/noticias/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Noticia
 
 def Listar(request):
@@ -18,3 +21,19 @@ def Listar(request):
 # EL TEMPLATE ya separa el diccionario
 # nombre = 'nicolas'
 # notas = [5,6,9]
+# 
+# #VISTA BASADA EN FUNCIONES
+@login_required
+def Detalle_Noticia(request, pk):
+	ctx = {}
+	noticia = Noticia.objects.get(pk = pk)
+	ctx['resultado'] = noticia
+	return render(request,'noticias/detalle_noticia.html', ctx)
+
+class Detalle_Noticia_Clase(LoginRequiredMixin, DetailView):
+	model = Noticia
+	template_name = 'noticias/detalle_noticia.html'
+
+#SI USO UNA VISTA BASADA EN CLASE EL CONTEXTO SE LLAMA:
+# SI ES UNO SOLO object
+# SI SON MUCHOS SE LLAMA obect_list
