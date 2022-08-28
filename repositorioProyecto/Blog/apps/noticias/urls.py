4d6b7b29d56e24fca5508f1b5fb0f4315f6f57ab
Nicolas Ortiz Kovalek <105877391+nikovalek@users.noreply.github.com>
2022-08-28 15:18:46 -0300

    Botones

--- repositorioProyecto/Blog/apps/noticias/urls.py
+++ repositorioProyecto/Blog/apps/noticias/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     
     path('listar/', views.Listar, name = 'listar_noticias'),
 
+    path('Buscar/', views.Buscar, name = 'buscar_noticias'),
+
     # URL PARA VISTA BASADA EN FUNCION
     #path('detalle/<int:pk>', views.Detalle_Noticia_Funcion, name = 'detalle_noticias'),
     # URL PARA VISTA BASADA EN CLASE
--- repositorioProyecto/Blog/apps/noticias/views.py
+++ repositorioProyecto/Blog/apps/noticias/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 
 from .models import Noticia,Comentario,Categoria
+from django.db.models import Q
 
 def Listar(request):
 	#Creo el diccionario para pasar datos al temaplte
@@ -22,9 +23,22 @@ def Listar(request):
 		if categoria != "0":
 			noticias = noticias.filter(categoria=categoria)
 	ctx['categorias'] = Categoria.objects.all()
-        
 
 	return render(request,'noticias/listar_noticias.html',ctx)
+
+def Buscar(request):
+	#creo el filtro de busqueda
+	queryset= request.GET.get("buscar")
+	noticias= Noticia.objects.filter(estado = True)
+	if queryset :
+		noticias= Noticia.objects.filter(
+			Q(Categoria__icontains= queryset) |
+			Q(Titulo__icontains= queryset)
+		).distinct()
+	return render(request, 'noticias/listar_noticias.html', {'Noticias': noticias})
+
+
+        
 
 # EJEMPLO DE COMO DESARMA EL CTX EL TEMPLATE.
 # ctx['nombre'] = 'nicolas'
@@ -61,4 +75,25 @@ def Agregar_Comentario(request,pk):
 	c = Comentario.objects.create(noticia = noti, texto = texto_comentario, usuario = request.user)
 
 	return HttpResponseRedirect(reverse_lazy('noticias:detalle_noticias' , kwargs={'pk':pk}))
+	
+#Ejemplo de Dani
+def busqueda_categorias(request):
 
+	return render(request, "noticias/buscar_categoria.html")
+
+def Buscar(request):
+
+	if request.GET["prd"]:
+
+		mensaje="Categoria buscada: %r" %request.GET["prd"] 
+
+		return HttpResponse(mensaje)
+		#noticia=request.GET["prd"]
+
+		#categoria=categoria.objects.filter(nombre_icontains=producto)
+
+	else:
+
+		mensaje="No has introducido nada" 
+	
+	return HttpResponse(mensaje)
